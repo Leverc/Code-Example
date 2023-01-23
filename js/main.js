bb6dd00e21a04f55f0e13cbc62cb175f354d0c82
Stephen Hill <stephenhill30@gmail.com>
2023-01-23 18:33:39 -0500

    Played with css and js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Script loaded");
+
+button = document.querySelector("button");
+button.onclick = () => {
+    console.log("YOU CLICKED ME");
+    window.location.href = "https://google.com";
+
+};
